b804181eb5dcf4a621c1f90354bb20e0f7dd9851
farras13 <farrasgopal13@gmail.com>
2023-04-05 05:13:27 +0700

    create News

--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Helpers\ApiFormatter;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\NewsRequest;
 use App\Models\News;
 use Illuminate\Http\Request;
 
@@ -40,9 +41,22 @@ class NewsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(NewsRequest $request)
     {
-        //
+        $file = $request->file('images');
+
+        $nama_file = time() . "_" . $file->getClientOriginalName();
+
+        $tujuan_upload = 'data_file';
+        $file->move($tujuan_upload, $nama_file);
+        $data = [
+            'title' => $request->title,
+            'content' => $request->content,
+            'image' => $nama_file,
+        ];
+        $post = News::create($data);
+        if ($post) return ApiFormatter::createApi(200, 'success insert data', $data);
+        return ApiFormatter::createApi(500, 'failed insert data', $data);
     }
 
     /**
